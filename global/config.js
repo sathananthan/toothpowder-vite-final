9af741be787a6b37ee09d444505fb5fb6302f7c1
sathananthan <nahtnanahtas2002@gmail.com>
2024-06-15 00:48:07 +0530

    connected to render

--- global/config.js
+++ global/config.js
@@ -1,3 +1,3 @@
 /*eslint-disable*/
-const API_URL = "http://localhost:3001/";   //https://tooth-backend.onrender.com/
+const API_URL = "https://tooth-backend.onrender.com/"; //http://localhost:3001/
 export default API_URL;
