85684380ae184e509bbea6eebd15e36c7aef133a
sathananthan <nahtnanahtas2002@gmail.com>
2024-11-07 21:31:41 +0530

    black with logo and brand name

--- src/pages/Navbar1.jsx
+++ src/pages/Navbar1.jsx
@@ -9,7 +9,8 @@ import Inventory2Icon from "@mui/icons-material/Inventory2";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
-import CompanyIcon from "../assets/w109h110.png";
+// import CompanyIcon from "../assets/w109h110.png";
+import CompanyIcon from "../assets/logo3 croped1.png";
 // import AssignmentIcon from "@mui/icons-material/Assignment";
 
 export default function Navbar1() {
@@ -54,7 +55,7 @@ export default function Navbar1() {
           </Navbar.Brand>
           <Nav.Link as={Link} to="/cart" className="cent ms-auto">
             <ShoppingCartIcon style={{ fontSize: "35px", marginTop: "7px" }} />
-            <Badge bg="dark">{basket?.length}</Badge>
+            <Badge bg="danger">{basket?.length}</Badge>
           </Nav.Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -127,7 +128,7 @@ export default function Navbar1() {
                 <ShoppingCartIcon
                   style={{ fontSize: "35px", marginTop: "7px" }}
                 />
-                <Badge bg="dark">{basket?.length}</Badge>
+                <Badge bg="danger">{basket?.length}</Badge>
               </Nav.Link>
               {/* )} */}
             </Nav>
