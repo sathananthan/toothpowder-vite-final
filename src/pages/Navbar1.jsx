8db884c401e86a078b9039bd21484de49b258ca4
sathananthan <nahtnanahtas2002@gmail.com>
2024-11-07 13:32:26 +0530

    orange with logo brand name added

--- src/pages/Navbar1.jsx
+++ src/pages/Navbar1.jsx
@@ -9,6 +9,7 @@ import Inventory2Icon from "@mui/icons-material/Inventory2";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import CompanyIcon from "../assets/w109h110.png";
 // import AssignmentIcon from "@mui/icons-material/Assignment";
 
 export default function Navbar1() {
@@ -36,16 +37,24 @@ export default function Navbar1() {
   return (
     <>
       <Navbar
-        bg="dark"
-        data-bs-theme="dark"
+        /*  bg="dark"
+        data-bs-theme="dark" */
         expand="lg" /* for responsive */
         className="navbar-custom"
       >
         <Container>
-          <Navbar.Brand href="/">Logo</Navbar.Brand>
+          <Navbar.Brand>
+            <div className="brand-container">
+              <img
+                src={CompanyIcon} // Replace with your logo path
+                className="logo-custom"
+              />
+              <span className="brand-name">AAT</span>
+            </div>
+          </Navbar.Brand>
           <Nav.Link as={Link} to="/cart" className="cent ms-auto">
             <ShoppingCartIcon style={{ fontSize: "35px", marginTop: "7px" }} />
-            <Badge bg="danger">{basket?.length}</Badge>
+            <Badge bg="dark">{basket?.length}</Badge>
           </Nav.Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -118,7 +127,7 @@ export default function Navbar1() {
                 <ShoppingCartIcon
                   style={{ fontSize: "35px", marginTop: "7px" }}
                 />
-                <Badge bg="danger">{basket?.length}</Badge>
+                <Badge bg="dark">{basket?.length}</Badge>
               </Nav.Link>
               {/* )} */}
             </Nav>
